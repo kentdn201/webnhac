08a8eb040e6c042bd66e38b2145cddb15afb0fda
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-04 15:19:49 +0700

    Update conn.php

--- inc/conn.php
+++ inc/conn.php
@@ -1,17 +1,4 @@
 <?php	 
 	$conn = pg_connect("host=ec2-52-21-252-142.compute-1.amazonaws.com dbname=dcle2fpc522se2 user=fmneqfpwrsmfic password=f2f4b403db219824f6af70a62a6cd17f982167cdc23ef004cf3ed53e403f98f4 port=5432");
 	if(!$conn){echo "Lost Connect";}
-	if(isset($_POST['login'])){
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-		$sql= "SELECT * FROM account WHERE username ='$username' and password='$password'";
-		$result = pg_query($conn, $sql);
-		$check = pg_num_rows($result);
-		if($check == 1){
-			echo "Vao dc roi";
-		}
-		else{
-			echo "Vao lai di";
-		}
-	}
  ?>
