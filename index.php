b189778b5390f394d2221c81c3a557b6f74eb4a9
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-05 21:53:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -82,22 +82,6 @@
 		</div>
 	</div>
 	<body>
-    <footer>
-        <div class="footer-content">
-            <h3>Code Opacity</h3>
-            <p>Demo 123 ádjlkasdjasdlajskldalkdjklsajdklasjdlksajdlksajdlksajd</p>
-            <ul class="socials">
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-            </ul>
-        </div>
-        <div class="footer-bottom">
-            No copyright :D
-        </div>
-    </footer>
 </body>
 </body>
 </html>
