2d8a34b8f75b3cfbc6a1d74f393d655b69b1add4
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-05 23:39:06 +0700

    Update header.php

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -7,28 +7,19 @@
 	        </button>
         </div>
 				<div class="p-4 pt-5">
-		  		<h1><a href="index.php" class="logo">NamMP3</a></h1>
+		  		<h1><a href="index.php" class="logo">ATN ToyStore</a></h1>
 	        <ul class="list-unstyled components mb-5">
 	          <li class="active">
-	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Song Manager</a>
+	            <a href="list-product.php" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Product Manager</a>
 	            <ul class="collapse list-unstyled" id="homeSubmenu">
                 <li>
-                    <a href="list-song.php">Song</a>
+                    <a href="list-product.php">Song</a>
                 </li>
                 <li>
-                  <a href="add-song.php">Add</a>
+                  <a href="add-product.php">Add</a>
                 </li>
 	            </ul>
 	          </li>
-	          	<li>
-	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Account Manager</a>
-	            <ul class="collapse list-unstyled" id="homeSubmenu">
-                <li>
-                    <a href="list-account.php">Account</a>
-                </li>
-                <li>
-                  <a href="add-account.php">Add</a>
-                </li>
 	            </ul>
 	          </li>
 	          <li>
