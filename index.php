85f5c27a1a401d7b5e34721db8d4fa0c94df2339
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-05 15:39:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,6 @@
 <body>
 	<div class="container">
 		<?php
-			require_once("inc/conn.php")
 			include("inc/header.php")
 		?>
 		<div id="carouselExampleIndicators" class="carousel slide mt-1" data-ride="carousel">
@@ -64,38 +63,6 @@
 	<div class="container">
 		<div class="col-xs-12 col-sm-8 col-md-12">
 			<h2 style="background-color: black; font-size: 23px; color: white; padding: 10px;">Những bài hát mới nhất</h2>
-			<?php
-			if(!empty($_GET['page']))
-		        {
-		          $page=$_GET['page']-1;
-		        }
-		        else
-		        {
-		          $page =0;
-		        }
-    			//$page = !empty($_GET['page']) ? ($_GET['page'] - 1): 0 ; //lay page hien tai
-      			$product_per_page = 4; //1 trang 3 sp 
-      			$offset = $product_per_page * $page; //offset chinh la phan can bo qua 
-		      $sql = "SELECT * FROM music LIMIT $offset,$product_per_page ";
-		      $rs = mysqli_query($conn, $sql);
-
-		      if(mysqli_num_rows($rs) > 0 ){
-		        while( $row = mysqli_fetch_assoc($rs) ){
-		      ?>
-		      	<div>
-			        <a href="single-product.php?id=<?php echo $row['id']?>" class="product">
-			            <div class="product-image"><img src="images/<?php echo $row['anh'] ?>" /></div>
-			            <h2 class="product-title"><?php echo $row['name'] ?></h2>
-			            <p><?php echo $row['casi']?></p>
-	 				</a>
- 				</div>
-		    <?php 
-
-		        }//end while 
-
-		      }//check so hang tra ve > 0 
-
-		?>
 		</div>
 	</div>
 	<div class="hot-singer container col-xs-12 col-xs-12 navbar" style="background-color:white;">
