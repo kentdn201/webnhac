6dc58949143da16f518901a7da2713d72a6854a6
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-05 22:10:04 +0700

    Update conn.php

--- admin/inc/conn.php
+++ admin/inc/conn.php
@@ -1,13 +1,4 @@
-<?php
-	$servername="localhost";
-	$username="root";
-	$password="";
-	$dbname="id14992072_music";
-
-	$conn= mysqli_connect($servername,$username,$password,$dbname);
-
-	if(!$conn)
-	{
-		die("Connect Fail". mysql_connect_error());
-	}
-?>
+<?php	 
+	$conn = pg_connect("host=ec2-52-21-252-142.compute-1.amazonaws.com dbname=dcle2fpc522se2 user=fmneqfpwrsmfic password=f2f4b403db219824f6af70a62a6cd17f982167cdc23ef004cf3ed53e403f98f4 port=5432");
+	if(!$conn){echo "Lost Connect";}
+ ?>
