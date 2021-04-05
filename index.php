338ed646d00928cb7097754dd9ca9fa82632b19e
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-05 16:31:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<?php	 
- $conn = pg_connect("host=ec2-52-21-252-142.compute-1.amazonaws.com dbname=dcle2fpc522se2 user=fmneqfpwrsmfic password=f2f4b403db219824f6af70a62a6cd17f982167cdc23ef004cf3ed53e403f98f4 port=5432");
- if(!$conn){echo "Lost Connect";}
- if(isset($_POST['login'])){
-	$username = $_POST['username'];
- 	$password = $_POST['password'];
- 	if ($username == "admin" && $password == "admin"){
-		echo"ok";
-	}
- }
-		
-
- ?>
-
 <!DOCTYPE html>
 <html>
 <head>
