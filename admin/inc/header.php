49eca2e6cf5051401785b474c2e4e6a35ef8420e
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-07 14:02:19 +0700

    Update header.php

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -7,7 +7,7 @@
 	        </button>
         </div>
 				<div class="p-4 pt-5">
-		  		<h1><a href="index.php" class="logo">NamMP3</a></h1>
+		  		<h1><a href="index.php" class="logo">ATN</a></h1>
 	        <ul class="list-unstyled components mb-5">
 	        <li class="active">
 	        <a href="list-product.php" data-toggle="collapse" aria-expanded="false">Product Manager</a>
