7e9c0ef17c0288dfd0fc48df631715ecece3a75e
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-07 11:02:09 +0700

    Update header.php

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -6,21 +6,22 @@
 	          <span class="sr-only">Toggle Menu</span>
 	        </button>
         </div>
-				<div class="p-4 pt-5">
-		  		<h1><a href="index.php" class="logo">ATN</a></h1>
+		<div class="p-4 pt-5">
+		<h1><a href="index.php" class="logo">ATN</a></h1>
 	        <ul class="list-unstyled components mb-5">
 	          <li class="active">
 	            <a href="list-product.php" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Song Manager</a>
 	            <ul class="collapse list-unstyled" id="homeSubmenu">
-                <li>
-                    <a href="list-product.php">Song</a>
-                </li>
-                <li>
-                  <a href="add-product.php">Add</a>
-                </li>
+			<li>
+			    <a href="list-product.php">Song</a>
+			</li>
+			<li>
+			  <a href="add-product.php">Add</a>
+			</li>
 	            </ul>
 	          </li>
-	          	<li>
+		</ul>
+	          <li>
 	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Account Manager</a>
 	            <ul class="collapse list-unstyled" id="homeSubmenu">
                 <li>
