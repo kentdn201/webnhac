842eac3e37f3811a046ba237a14c248b68b768b9
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-07 11:23:48 +0700

    Update header.php

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,4 +1,4 @@
-<div class="wrapper d-flex align-items-stretch">
+    <div class="wrapper d-flex align-items-stretch">
 			<nav id="sidebar">
 				<div class="custom-menu">
 					<button type="button" id="sidebarCollapse" class="btn btn-primary">
@@ -7,19 +7,17 @@
 	        </button>
         </div>
 				<div class="p-4 pt-5">
-		  		<h1><a href="index.php" class="logo">ATN ToyStore</a></h1>
+		  		<h1><a href="index.php" class="logo">NamMP3</a></h1>
 	        <ul class="list-unstyled components mb-5">
 	          <li class="active">
-	            <a href="list-product.php" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Product Manager</a>
+	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Song Manager</a>
 	            <ul class="collapse list-unstyled" id="homeSubmenu">
                 <li>
-                    <a href="list-product.php">Song</a>
+                    <a href="list-song.php">Song</a>
                 </li>
                 <li>
-                  <a href="add-product.php">Add</a>
+                  <a href="add-song.php">Add</a>
                 </li>
-	            </ul>
-	          </li>
 	            </ul>
 	          </li>
 	          <li>
