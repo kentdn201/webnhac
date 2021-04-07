69ecf71a2cd4fd35fe0f3e70059becfb9e0a9b61
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-07 11:17:39 +0700

    Update header.php

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,39 +1,29 @@
 <div class="wrapper d-flex align-items-stretch">
-	<nav id="sidebar">
-		<div class="custom-menu">
-			<button type="button" id="sidebarCollapse" class="btn btn-primary">
-	        	<i class="fa fa-bars"></i>
-	          	<span class="sr-only">Toggle Menu</span>
+			<nav id="sidebar">
+				<div class="custom-menu">
+					<button type="button" id="sidebarCollapse" class="btn btn-primary">
+	          <i class="fa fa-bars"></i>
+	          <span class="sr-only">Toggle Menu</span>
 	        </button>
         </div>
-		<div class="p-4 pt-5">
-		<h1><a href="index.php" class="logo">ATN</a></h1>
+				<div class="p-4 pt-5">
+		  		<h1><a href="index.php" class="logo">ATN ToyStore</a></h1>
 	        <ul class="list-unstyled components mb-5">
-	        <li class="active">
-	            <a href="list-product.php" data-toggle="collapse" aria-expanded="false">Product Manager</a>
+	          <li class="active">
+	            <a href="list-product.php" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Product Manager</a>
 	            <ul class="collapse list-unstyled" id="homeSubmenu">
-			<li>
-			    <a href="list-product.php">Song</a>
-			</li>
-			<li>
-			  <a href="add-product.php">Add</a>
-			</li>
-	        	</ul>
+                <li>
+                    <a href="list-product.php">Song</a>
+                </li>
+                <li>
+                  <a href="add-product.php">Add</a>
+                </li>
+	            </ul>
 	          </li>
-	          <li>
-	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Account Manager</a>
-	            <ul class="collapse list-unstyled" id="homeSubmenu">
-                <li>
-                    <a href="list-account.php">Account</a>
-                </li>
-                <li>
-                  <a href="add-account.php">Add</a>
-                </li>
 	            </ul>
 	          </li>
 	          <li>
               <a href="logout.php">Logout</a>
 	        </ul>
-	    </div>
-	</nav>
-</div>
+	      </div>
+	  </nav>
