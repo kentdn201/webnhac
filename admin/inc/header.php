0fff1deceef9f5c59c33af4da102323632fda09c
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-07 11:12:10 +0700

    Update header.php

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,15 +1,15 @@
 <div class="wrapper d-flex align-items-stretch">
-			<nav id="sidebar">
-				<div class="custom-menu">
-					<button type="button" id="sidebarCollapse" class="btn btn-primary">
-	          <i class="fa fa-bars"></i>
-	          <span class="sr-only">Toggle Menu</span>
+	<nav id="sidebar">
+		<div class="custom-menu">
+			<button type="button" id="sidebarCollapse" class="btn btn-primary">
+	        	<i class="fa fa-bars"></i>
+	          	<span class="sr-only">Toggle Menu</span>
 	        </button>
         </div>
 		<div class="p-4 pt-5">
 		<h1><a href="index.php" class="logo">ATN</a></h1>
 	        <ul class="list-unstyled components mb-5">
-	          <li class="active">
+	        <li class="active">
 	            <a href="list-product.php" data-toggle="collapse" aria-expanded="false">Product Manager</a>
 	            <ul class="collapse list-unstyled" id="homeSubmenu">
 			<li>
@@ -18,7 +18,7 @@
 			<li>
 			  <a href="add-product.php">Add</a>
 			</li>
-	            </ul>
+	        	</ul>
 	          </li>
 	          <li>
 	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Account Manager</a>
@@ -34,5 +34,6 @@
 	          <li>
               <a href="logout.php">Logout</a>
 	        </ul>
-	      </div>
-	  </nav>
+	    </div>
+	</nav>
+</div>
