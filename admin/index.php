6688c788d1ae05d46c10876809950ff6a7f4c786
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-09 14:41:33 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -4,10 +4,11 @@
  ?>
 <?php
 	include("inc/header.php");
+?>
 if( $_SERVER['REQUEST_METHOD'] =='GET' && !EMPTY($_GET['idxoa'])){
 	$idxoa = $_GET['idxoa'];
 	$sql = "DELETE FROM product WHERE product_id = {$idxoa}";
-	if( mysqli_query($conn, $sql)){
+	if( pg_query($conn, $sql)){
 		echo "Delete product have ID: " .$idxoa ." successful <br>";
 	}else{
 		echo "Delete error: " . mysqli_error($conn);
