920b7b8b9e0b5f99d2b3db2d6bf5cdeafdcf76a4
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-09 14:39:08 +0700

    Update conn.php

--- admin/inc/conn.php
+++ admin/inc/conn.php
@@ -1,11 +1,4 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$pass = "";
-$dbname = "tunesource2";
-$conn = mysqli_connect($servername, $username, $pass, $dbname);
-if(!$conn){
-	die(" connection error"). mysqli_connect_error();
-}
-echo "";
-?>
+<?php	 
+	$conn = pg_connect("host=ec2-52-21-252-142.compute-1.amazonaws.com dbname=dcle2fpc522se2 user=fmneqfpwrsmfic password=f2f4b403db219824f6af70a62a6cd17f982167cdc23ef004cf3ed53e403f98f4 port=5432");
+	if(!$conn){echo "Lost Connect";}
+ ?>
