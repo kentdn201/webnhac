a2842e02a744b1e7271c0f7f5b0841487653f2b5
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-09 16:34:31 +0700

    Update header.php

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,13 +1,12 @@
     <div class="wrapper d-flex align-items-stretch">
-			<nav id="sidebar">
-				<div class="custom-menu">
-					<button type="button" id="sidebarCollapse" class="btn btn-primary">
-	          <i class="fa fa-bars"></i>
-	          <span class="sr-only">Toggle Menu</span>
-	        </button>
-        </div>
-				<div class="p-4 pt-5">
-		  		<h1><a href="index.php" class="logo">ATN</a></h1>
+	<nav id="sidebar">
+		<div class="custom-menu">
+			<button type="button" id="sidebarCollapse" class="btn btn-primary">
+	          	<i class="fa fa-bars"></i>
+	        	</button>
+   </div>
+	<div class="p-4 pt-5">
+		<h1><a href="index.php" class="logo">ATN</a></h1>
 	        <ul class="list-unstyled components mb-5">
 	        <li class="active">
 	        <a href="list-product.php" data-toggle="collapse" aria-expanded="false">Product Manager</a>
