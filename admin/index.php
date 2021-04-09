bffd9b41c49c5d2760bb66c221af2c726957d9c6
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-09 14:45:35 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -5,15 +5,19 @@
 <?php
 	include("inc/header.php");
 ?>
-if( $_SERVER['REQUEST_METHOD'] =='GET' && !EMPTY($_GET['idxoa'])){
-	$idxoa = $_GET['idxoa'];
-	$sql = "DELETE FROM product WHERE product_id = {$idxoa}";
-	if( pg_query($conn, $sql)){
-		echo "Delete product have ID: " .$idxoa ." successful <br>";
-	}else{
-		echo "Delete error: " . mysqli_error($conn);
-	}
+<?php
+      				 if( $_SERVER['REQUEST_METHOD']=='GET' && !empty( $_GET['idxoa'])){
 
-}
+				    $idxoa = $_GET['idxoa'];
+
+				    $sql ="DELETE FROM product WHERE productid={$idxoa}";
+				    if(pg_query($conn, $sql)){
+				     echo "Delete Success Product ID: ".$idxoa;
+				    } else{
+				    echo "Error" .pg_error($conn);
+				    }
+				  }
+
+      			?>
 include ("inc/header.php");
 include ("listsp.php");
