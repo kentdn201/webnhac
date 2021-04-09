d74a7b8bbe67d9d7ab7e7c1b759521ee98ccb784
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-09 17:04:04 +0700

    Update indexpre.php

--- indexpre.php
+++ indexpre.php
@@ -23,9 +23,9 @@
     <link rel="stylesheet" href="css/font-awesome.min.css">
     
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css_theindexfall/owl.carousel.css">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css_theindexfall/responsive.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
