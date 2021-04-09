e650fc67a8068c086eafc9dcf0cfc71196889c9a
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-09 15:12:06 +0700

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -9,9 +9,6 @@
 	      <li class="nav-item active">
 	        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
 	      </li>
-	      <li>
-	      	<a class="nav-link" href="admin/login.php">Admin</a>
-	      </li>
 	      <li class="nav-item dropdown">
 	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 	          Type
