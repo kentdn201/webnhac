6f8469c70e5f597b2bc5037e4163a62ff5ab9053
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-10 15:48:26 +0700

    Update single-product.php

--- single-product.php
+++ single-product.php
@@ -32,7 +32,7 @@
             <div class="row">
                 <div class="col-sm-6">
                     <div class="logo">
-                        <h1><a href="./"><img src="img/logo.png"></a></h1>
+                        <h1><a href="./"><img src="image/logo.jpg"></a></h1>
                     </div>
                 </div>
                 
@@ -61,7 +61,6 @@
                     <ul class="nav navbar-nav">
                         <li><a href="index.html">Home</a></li>
                         <li><a href="shop.html">Shop page</a></li>
-                        <li class="active"><a href="single-product.html">Single product</a></li>
                         <li><a href="cart.html">Cart</a></li>
                         <li><a href="checkout.html">Checkout</a></li>
                     </ul>
@@ -91,7 +90,7 @@
                 <div class="col-md-4">
                     <div class="single-sidebar">
                         <h2 class="sidebar-title">Search Products</h2>
-                        <form action="">
+                        <form action="search.php">
                             <input type="text" placeholder="Search products...">
                             <input type="submit" value="Search">
                         </form>
@@ -148,27 +147,34 @@
                             <a href="">Category Name</a>
                             <a href="">Sony Smart TV - 2015</a>
                         </div>
-                        
                         <div class="row">
+                            <?php
+                                $id = $_GET['id'];
+
+                                $sql = "SELECT * FROM music WHERE id = {$id}";
+                                $rs = pg_query($conn, $sql);
+                                while ( $row = mysqli_fetch_assoc($rs) ){   
+                            ?>
                             <div class="col-sm-6">
                                 <div class="product-images">
                                     <div class="product-main-img">
-                                        <img src="img/product-2.jpg" alt="">
+                                        <img "images/<?php echo $row['image']?>
                                     </div>
                                     
                                     <div class="product-gallery">
-                                        <img src="img/product-thumb-1.jpg" alt="">
-                                        <img src="img/product-thumb-2.jpg" alt="">
-                                        <img src="img/product-thumb-3.jpg" alt="">
+                                        <img "images/<?php echo $row['image']?>
+                                        <img "images/<?php echo $row['image']?>
+                                        <img "images/<?php echo $row['image']?>
                                     </div>
                                 </div>
                             </div>
                             
                             <div class="col-sm-6">
                                 <div class="product-inner">
-                                    <h2 class="product-name">Sony Smart TV - 2015</h2>
+                                    <h2 class="product-name"><?php echo $row['name']?></h2>
                                     <div class="product-inner-price">
-                                        <ins>$700.00</ins> <del>$100.00</del>
+                                        <?php echo $row['price'] . "$"?>
+                                        <!--<ins>$700.00</ins> <del>$100.00</del>-->
                                     </div>    
                                     
                                     <form action="" class="cart">
@@ -177,11 +183,6 @@
                                         </div>
                                         <button class="add_to_cart_button" type="submit">Add to cart</button>
                                     </form>   
-                                    
-                                    <div class="product-inner-category">
-                                        <p>Category: <a href="">Summer</a>. Tags: <a href="">awesome</a>, <a href="">best</a>, <a href="">sale</a>, <a href="">shoes</a>. </p>
-                                    </div> 
-                                    
                                     <div role="tabpanel">
                                         <ul class="product-tab" role="tablist">
                                             <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Description</a></li>
@@ -189,10 +190,7 @@
                                         </ul>
                                         <div class="tab-content">
                                             <div role="tabpanel" class="tab-pane fade in active" id="home">
-                                                <h2>Product Description</h2>  
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique, diam in consequat iaculis, est purus iaculis mauris, imperdiet facilisis ante ligula at nulla. Quisque volutpat nulla risus, id maximus ex aliquet ut. Suspendisse potenti. Nulla varius lectus id turpis dignissim porta. Quisque magna arcu, blandit quis felis vehicula, feugiat gravida diam. Nullam nec turpis ligula. Aliquam quis blandit elit, ac sodales nisl. Aliquam eget dolor eget elit malesuada aliquet. In varius lorem lorem, semper bibendum lectus lobortis ac.</p>
-
-                                                <p>Mauris placerat vitae lorem gravida viverra. Mauris in fringilla ex. Nulla facilisi. Etiam scelerisque tincidunt quam facilisis lobortis. In malesuada pulvinar neque a consectetur. Nunc aliquam gravida purus, non malesuada sem accumsan in. Morbi vel sodales libero.</p>
+                                                <?php echo $row['description'] . "$"?>
                                             </div>
                                             <div role="tabpanel" class="tab-pane fade" id="profile">
                                                 <h2>Reviews</h2>
@@ -220,8 +218,9 @@
                                 </div>
                             </div>
                         </div>
-                        
-                        
+                        <?php
+							}
+						?>
                         <div class="related-products-wrapper">
                             <h2 class="related-products-title">Related Products</h2>
                             <div class="related-products-carousel">
