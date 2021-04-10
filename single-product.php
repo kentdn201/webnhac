eee05dbe6034c98102553661c6cbb30c38265ba8
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-10 15:55:12 +0700

    Update single-product.php

--- single-product.php
+++ single-product.php
@@ -32,7 +32,7 @@
             <div class="row">
                 <div class="col-sm-6">
                     <div class="logo">
-                        <h1><a href="index.php"><img src="image/logo.png"></a></h1>
+                        <h1><a href="index.php"><img src="images/logo.png"></a></h1>
                     </div>
                 </div>
                 
