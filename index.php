3291d72b291f3d35c939e24fbcd7f7d8816614ec
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-11 20:45:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -46,8 +46,6 @@
 		  </a>
 		</div>
 		<br>
-	</div>
-	<div class="container">
 		<div class="col-xs-12 col-sm-8 col-md-12">
 		<h2 style="background-color: black; font-size: 23px; color: white; padding: 10px;">Best sales</h2>
 		<?php
@@ -84,82 +82,82 @@
 		?>
 		</div>
 	</div>
-<div class="footer-top-area">
-   <div class="zigzag-bottom"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-about-us">
-                        <h2><span>ATN</span></h2>
-                        <p>This is ATN Store</p>
-                        <div class="footer-social">
-                            <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-menu">
-                        <h2 class="footer-wid-title">User Navigation </h2>
-                        <ul>
-                            <li><a href="">My account</a></li>
-                            <li><a href="">Order history</a></li>
-                            <li><a href="">Wishlist</a></li>
-                            <li><a href="">Vendor contact</a></li>
-                            <li><a href="">Front page</a></li>
-                        </ul>                        
-                    </div>
-                </div>
-                
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-menu">
-                        <h2 class="footer-wid-title">Categories</h2>
-                        <ul>
-                            <li><a href="">Mobile Phone</a></li>
-                            <li><a href="">Home accesseries</a></li>
-                            <li><a href="">LED TV</a></li>
-                            <li><a href="">Computer</a></li>
-                            <li><a href="">Gadets</a></li>
-                        </ul>                        
-                    </div>
-                </div>
-                
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-newsletter">
-                        <h2 class="footer-wid-title">Newsletter</h2>
-                        <p>Sign up to our newsletter and get exclusive deals you wont find anywhere else straight to your inbox!</p>
-                        <div class="newsletter-form">
-                            <input type="email" placeholder="Type your email">
-                            <input type="submit" value="Subscribe">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="footer-bottom-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    <div class="copyright">
-                       <p>&copy; 2015 uCommerce. All Rights Reserved. <a href="http://www.freshdesignweb.com" target="_blank">freshDesignweb.com</a></p>
-                    </div>
-                </div>
-                
-                <div class="col-md-4">
-                    <div class="footer-card-icon">
-                        <i class="fa fa-cc-discover"></i>
-                        <i class="fa fa-cc-mastercard"></i>
-                        <i class="fa fa-cc-paypal"></i>
-                        <i class="fa fa-cc-visa"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="footer-top-area">
+	   <div class="zigzag-bottom"></div>
+		<div class="container">
+		    <div class="row">
+			<div class="col-md-3 col-sm-6">
+			    <div class="footer-about-us">
+				<h2><span>ATN</span></h2>
+				<p>This is ATN Store</p>
+				<div class="footer-social">
+				    <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+				    <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+				    <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
+				    <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+				</div>
+			    </div>
+			</div>
+
+			<div class="col-md-3 col-sm-6">
+			    <div class="footer-menu">
+				<h2 class="footer-wid-title">User Navigation </h2>
+				<ul>
+				    <li><a href="">My account</a></li>
+				    <li><a href="">Order history</a></li>
+				    <li><a href="">Wishlist</a></li>
+				    <li><a href="">Vendor contact</a></li>
+				    <li><a href="">Front page</a></li>
+				</ul>                        
+			    </div>
+			</div>
+
+			<div class="col-md-3 col-sm-6">
+			    <div class="footer-menu">
+				<h2 class="footer-wid-title">Categories</h2>
+				<ul>
+				    <li><a href="">Mobile Phone</a></li>
+				    <li><a href="">Home accesseries</a></li>
+				    <li><a href="">LED TV</a></li>
+				    <li><a href="">Computer</a></li>
+				    <li><a href="">Gadets</a></li>
+				</ul>                        
+			    </div>
+			</div>
+
+			<div class="col-md-3 col-sm-6">
+			    <div class="footer-newsletter">
+				<h2 class="footer-wid-title">Newsletter</h2>
+				<p>Sign up to our newsletter and get exclusive deals you wont find anywhere else straight to your inbox!</p>
+				<div class="newsletter-form">
+				    <input type="email" placeholder="Type your email">
+				    <input type="submit" value="Subscribe">
+				</div>
+			    </div>
+			</div>
+		    </div>
+		</div>
+	    </div>
+	    <div class="footer-bottom-area">
+		<div class="container">
+		    <div class="row">
+			<div class="col-md-8">
+			    <div class="copyright">
+			       <p>&copy; 2015 uCommerce. All Rights Reserved. <a href="http://www.freshdesignweb.com" target="_blank">freshDesignweb.com</a></p>
+			    </div>
+			</div>
+
+			<div class="col-md-4">
+			    <div class="footer-card-icon">
+				<i class="fa fa-cc-discover"></i>
+				<i class="fa fa-cc-mastercard"></i>
+				<i class="fa fa-cc-paypal"></i>
+				<i class="fa fa-cc-visa"></i>
+			    </div>
+			</div>
+		    </div>
+		</div>
+	    </div>
    
     <!-- Latest jQuery form server -->
     <script src="https://code.jquery.com/jquery.min.js"></script>
