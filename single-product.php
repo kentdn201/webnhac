c90244210726b2b4806f67174c344091153ab7f1
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-11 15:49:20 +0700

    Update single-product.php

--- single-product.php
+++ single-product.php
@@ -69,20 +69,6 @@
         </div>
     </div> 
     <!-- End mainmenu area -->
-    
-    <div class="product-big-title-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="product-bit-title text-center">
-                        <h2>Shop</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    
-    
     <div class="single-product-area">
         <div class="zigzag-bottom"></div>
         <div class="container">
