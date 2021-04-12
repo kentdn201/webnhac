0027cac1dd1e64ea94a91c4b1a2fab000062af57
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-12 17:15:37 +0700

    Update single-product.php

--- single-product.php
+++ single-product.php
@@ -27,47 +27,9 @@
 
   </head>
   <body>
-    <div class="site-branding-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="logo">
-                        <a href="index.php"><img src="images/logo.png"></a>
-                    </div>
-                </div>
-                
-                <div class="col-sm-6">
-                    <div class="shopping-item">
-                        <a href="cart.html">Cart - <span class="cart-amunt">$100</span> <i class="fa fa-shopping-cart"></i> <span class="product-count">5</span></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- End site branding area -->
-    
-    <div class="mainmenu-area">
-        <div class="container">
-            <div class="row">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div> 
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="shop.php">Shop page</a></li>
-                        <li><a href="cart.php">Cart</a></li>
-                        <li><a href="checkout.html">Checkout</a></li>
-                    </ul>
-                </div>  
-            </div>
-        </div>
-    </div> 
+    <?php
+	include("inc/header.php");  
+	?>
     <!-- End mainmenu area -->
     <div class="single-product-area">
         <div class="zigzag-bottom"></div>
