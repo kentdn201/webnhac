d36ebb4e316c8ede847a71a5e4d4195dfec26416
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-12 17:16:31 +0700

    Update single-product.php

--- single-product.php
+++ single-product.php
@@ -36,13 +36,6 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <div class="single-sidebar">
-                        <h2 class="sidebar-title">Search Products</h2>
-                        <form action="search.php">
-                            <input type="text" placeholder="Search products...">
-                            <input type="submit" value="Search">
-                        </form>
-                    </div>
                     <?php
                         $id = $_GET['id'];
                         $sql = "SELECT * FROM product WHERE productid = {$id}";
