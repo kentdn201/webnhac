0bacc7357fc648a7323a74c86f0a511017cc6130
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-12 16:50:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -43,30 +43,32 @@
 		  </a>
 		</div>
    	</div>
-	<div class="container">
-		<div class="col-xs-12 col-sm-8 col-md-12">
-			<h2 style="background-color: black; font-size: 23px; color: white; padding: 10px;" class="container">Best sales</h2>
-			<?php
-		      	$sql = "SELECT * FROM product LIMIT 4";
-		      	$rs = pg_query($conn, $sql);
-		      if(pg_num_rows($rs) > 0 ){
-		        while( $row = pg_fetch_assoc($rs) ){
-		      ?>
-		      	<div>
-			        <a href="single-product.php?id=<?php echo $row['productid']?>" class="product">
-					<div class="product-image"><img src="images/<?php echo $row['image']?>"></div>
-					<h2 class="product-title"><?php echo $row['name']?></h2>
-					<p><?php echo $row['type']?></p>
-					<p><?php echo $row['price']?></p>
-					</a>
- 				</div>
-		    <?php 
+	<div class="products">
+		<div class="container">
+			<div class="col-xs-12 col-sm-8 col-md-12">
+				<h2 style="background-color: black; font-size: 23px; color: white; padding: 10px;" class="container">Best sales</h2>
+				<?php
+				$sql = "SELECT * FROM product LIMIT 4";
+				$rs = pg_query($conn, $sql);
+			      if(pg_num_rows($rs) > 0 ){
+				while( $row = pg_fetch_assoc($rs) ){
+			      ?>
+				<div>
+					<a href="single-product.php?id=<?php echo $row['productid']?>" class="product">
+						<div class="product-image"><img src="images/<?php echo $row['image']?>"></div>
+						<h2 class="product-title"><?php echo $row['name']?></h2>
+						<p><?php echo $row['type']?></p>
+						<p><?php echo $row['price']?></p>
+						</a>
+					</div>
+			    <?php 
 
-		        }//end while 
+				}//end while 
 
-		      }//check so hang tra ve > 0 
+			      }//check so hang tra ve > 0 
 
-		?>
+			?>
+			</div>
 		</div>
 	</div>
 </body>
