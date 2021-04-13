636b3bc3d69ced2549ca98838e1698daf94fbd23
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-13 13:28:24 +0700

    Update single-product.php

--- single-product.php
+++ single-product.php
@@ -1,165 +1,57 @@
-<?php
-	require_once("inc/conn.php");
-?>
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Single Product</title>
+<html>
+<head>
+	<title>Website</title>
+	<meta charset="utf-8">
+	<meta http-equiv="Content-type" content="text/html; charsetwgtf-g">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <!-- Google Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
-    
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/owl.carousel.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="css/home.css">
-    <link rel="stylesheet" href="css/responsive.css">
-
-  </head>
-  <body>
-	<div class="container">
+  	<link rel="stylesheet" type="text/css" href="css/home.css">
+</head>
+<body>
+<div class="container">
 	<?php
-		include("inc/header.php");  
-	?>
+	require_once("inc/conn.php");
+	include("inc/header.php")
+?>
+</div>
+	<div class="container">
+		<div class="single-product">
+				<?php 
+					$id = $_GET['id'];
+
+					$sql = "SELECT * FROM music WHERE id = {$id}";
+					$rs = mysqli_query($conn, $sql);
+
+					while ( $row = mysqli_fetch_assoc($rs) ){
+						?>
+						<div class="single-product">
+							<div class="product-image"><img src="images/<?php echo $row['anh']?>"></div>
+							<h2 class="product-title"><?php echo $row['name']?></h2>
+							<p><?php echo $row['casi']?></p>
+							<p><?php echo $row['price'] . "$"?></p>
+							<audio id='audio_1' controls preload loop controlsList="nodownload">
+		 						<source src='music/<?php echo $row['file']?>'/>
+		 					</audio>
+                            <br>
+                            <a href="buy-success.php?id=<?php echo $row['id']?>"><input type="submit" onclick="alert('Buy Success')" class="btn btn-danger" value="Pay"></a>
+							<div class="product-content">
+								<?php echo $row['lyric']?>
+							</div>
+							
+
+						<!-- end product content -->
+
+						<?php
+							}
+						?>	
+
+					</div>
+		</div>
 	</div>
-    <!-- End mainmenu area -->
-    <div class="single-product-area">
-        <div class="zigzag-bottom"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <?php
-                        $id = $_GET['id'];
-                        $sql = "SELECT * FROM product WHERE productid = {$id}";
-                        $rs = pg_query($conn, $sql);
-                        while($row = pg_fetch_assoc($rs) ){   
-                    ?>
-                    <div class="single-sidebar">
-                        <div class="w3-sidebar w3-light-grey w3-bar-block" style="width:8%">
-			  <a href="#" class="w3-bar-item w3-button">Link 1</a>
-			  <a href="#" class="w3-bar-item w3-button">Link 2</a>
-			  <a href="#" class="w3-bar-item w3-button">Link 3</a>
-			</div>
-                    </div>
-                
-                <div class="col-md-8">
-                    <div class="product-content-right">
-                        <div class="product-breadcroumb">
-                            <a href="">Home</a>
-                            <a href="">Category Name</a>
-                            <a href="">Sony Smart TV - 2015</a>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="product-images">
-                                    <div class="product-main-img">
-                                        <img src="images/<?php echo $row['image']?>">
-                                    </div>
-                                    
-                                    <div class="product-gallery">
-                                        <img src="images/<?php echo $row['image']?>">
-                                        <img src="images/<?php echo $row['image']?>">
-                                        <img src="images/<?php echo $row['image']?>">
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            <div class="col-sm-6">
-                                <div class="product-inner">
-                                    <h2 class="product-name"><?php echo $row['name']?></h2>
-                                    <div class="product-inner-price">
-                                        <?php echo $row['price'] . "$"?>
-                                        <!--<ins>$700.00</ins> <del>$100.00</del>-->
-                                    </div>    
-                                    
-                                    <form action="" class="cart">
-                                        <div class="quantity">
-                                            <input type="number" size="4" class="input-text qty text" title="Qty" value="1" name="quantity" min="1" step="1">
-                                        </div>
-                                        <button class="add_to_cart_button" type="submit">Add to cart</button>
-                                    </form>   
-                                    <div role="tabpanel">
-                                        <ul class="product-tab" role="tablist">
-                                            <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Description</a></li>
-                                        </ul>
-                                        <div class="tab-content">
-                                            <div role="tabpanel" class="tab-pane fade in active" id="home">
-                                                <?php echo $row['description'] . "$"?>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <?php
-				}
-			?>
-                    </div>                    
-                </div>
-            </div>
-        </div>
-    </div>
-<footer>
-	<div class="footer-dark">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6 col-md-3 item">
-                        <h3>Services</h3>
-                        <ul>
-                            <li><a href="#">Web design</a></li>
-                            <li><a href="#">Development</a></li>
-                            <li><a href="#">Hosting</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-6 col-md-3 item">
-                        <h3>About</h3>
-                        <ul>
-                            <li><a href="#">Company</a></li>
-                            <li><a href="#">Team</a></li>
-                            <li><a href="#">Careers</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-6 item text">
-                        <h3>Company Name</h3>
-                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
-                    </div>
-                    <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
-                </div>
-                <p class="copyright">Company Name © 2018</p>
-            </div>
-	</div>
-  </footer>
-   
-    <!-- Latest jQuery form server -->
-    <script src="https://code.jquery.com/jquery.min.js"></script>
-    
-    <!-- Bootstrap JS form CDN -->
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    
-    <!-- jQuery sticky menu -->
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.sticky.js"></script>
-    
-    <!-- jQuery easing -->
-    <script src="js/jquery.easing.1.3.min.js"></script>
-    
-    <!-- Main Script -->
-    <script src="js/main.js"></script>
-  </body>
+</div>
+</body>
 </html>
