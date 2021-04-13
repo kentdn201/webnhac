fd049de9643405b3f00445d6a610aafaa3ec4e94
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-13 16:16:34 +0700

    Update search.php

--- search.php
+++ search.php
@@ -46,35 +46,5 @@
 			</div>
 		</div>
 	</div>
-<footer>
-	<div class="footer-dark">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6 col-md-3 item">
-                        <h3>Services</h3>
-                        <ul>
-                            <li><a href="index.php">Homepage</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-6 col-md-3 item">
-                        <h3>About</h3>
-                        <ul>
-                            <li><a href="#">Company</a></li>
-                            <li><a href="#">Team</a></li>
-                            <li><a href="#">Careers</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-6 item text">
-                        <h3>ATN Store</h3>
-                        <p>This website selling products about toys</p>
-                    </div>
-                    <div class="col item social"><a href="facebook.com"><i class = "fab fa-facebook-f"></i></a><a href="facebook.com"><i class = "fab fa-facebook-f"></i></a></a><a href="facebook.com"><i class = "fab fa-facebook-f"></i></a><a href="instagram.com"><i class = "fab fa-whatsapp"></i></a></div>
-                </div>
-                <p class="copyright">Company Name © 2018</p>
-            </div>
-	</div>
-  </footer>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
