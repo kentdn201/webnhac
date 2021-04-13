15cb5c02a89811cce6dc6282a788ea55cd1f38cb
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-13 13:44:28 +0700

    Update single-product.php

--- single-product.php
+++ single-product.php
@@ -26,67 +26,64 @@
 <div class = "card-wrapper">
       <div class = "card">
         <!-- card left -->
+	      <?php 
+		$id = $_GET['id'];
+
+		$sql = "SELECT * FROM product WHERE productid = {$id}";
+		$rs = pg_query($conn, $sql);
+		while ( $row = mysqli_fetch_assoc($rs) ){
+              ?>
         <div class = "product-imgs">
           <div class = "img-display">
             <div class = "img-showcase">
-              <img src = "shoes_images/shoe_1.jpg" alt = "shoe image">
-              <img src = "shoes_images/shoe_2.jpg" alt = "shoe image">
-              <img src = "shoes_images/shoe_3.jpg" alt = "shoe image">
-              <img src = "shoes_images/shoe_4.jpg" alt = "shoe image">
+		<img src="images/<?php echo $row['image']?>" alt = "shoe image">
+		<img src="images/<?php echo $row['image']?>" alt = "shoe image">
+		<img src="images/<?php echo $row['image']?>" alt = "shoe image">
+		<img src="images/<?php echo $row['image']?>" alt = "shoe image">
             </div>
           </div>
           <div class = "img-select">
             <div class = "img-item">
               <a href = "#" data-id = "1">
-                <img src = "shoes_images/shoe_1.jpg" alt = "shoe image">
+                <img src="images/<?php echo $row['image']?>" alt = "shoe image">
               </a>
             </div>
             <div class = "img-item">
               <a href = "#" data-id = "2">
-                <img src = "shoes_images/shoe_2.jpg" alt = "shoe image">
+                <img src="images/<?php echo $row['image']?>" alt = "shoe image">
               </a>
             </div>
             <div class = "img-item">
               <a href = "#" data-id = "3">
-                <img src = "shoes_images/shoe_3.jpg" alt = "shoe image">
+                <img src="images/<?php echo $row['image']?>" alt = "shoe image">
               </a>
             </div>
             <div class = "img-item">
               <a href = "#" data-id = "4">
-                <img src = "shoes_images/shoe_4.jpg" alt = "shoe image">
+                <img src="images/<?php echo $row['image']?>" alt = "shoe image">
               </a>
             </div>
           </div>
         </div>
         <!-- card right -->
         <div class = "product-content">
-          <h2 class = "product-title">nike shoes</h2>
-          <a href = "#" class = "product-link">visit nike store</a>
+          <h2 class = "product-title"><?php echo $row['name']?></h2>
+          <a href = "index.php" class = "product-link">visit atn store</a>
           <div class = "product-rating">
             <i class = "fas fa-star"></i>
             <i class = "fas fa-star"></i>
             <i class = "fas fa-star"></i>
             <i class = "fas fa-star"></i>
             <i class = "fas fa-star-half-alt"></i>
-            <span>4.7(21)</span>
+            <span>4.6(19)</span>
           </div>
 
           <div class = "product-price">
-            <p class = "last-price">Old Price: <span>$257.00</span></p>
-            <p class = "new-price">New Price: <span>$249.00 (5%)</span></p>
+            <p class = "new-price"><?php echo $row['price'] . "$"?> (5%)</span></p>
           </div>
 
           <div class = "product-detail">
-            <h2>about this item: </h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eveniet veniam tempora fuga tenetur placeat sapiente architecto illum soluta consequuntur, aspernatur quidem at sequi ipsa!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, perferendis eius. Dignissimos, labore suscipit. Unde.</p>
-            <ul>
-              <li>Color: <span>Black</span></li>
-              <li>Available: <span>in stock</span></li>
-              <li>Category: <span>Shoes</span></li>
-              <li>Shipping Area: <span>All over the world</span></li>
-              <li>Shipping Fee: <span>Free</span></li>
-            </ul>
+            <?php echo $row['description'] . "$"?>
           </div>
 
           <div class = "purchase-info">
@@ -94,7 +91,6 @@
             <button type = "button" class = "btn">
               Add to Cart <i class = "fas fa-shopping-cart"></i>
             </button>
-            <button type = "button" class = "btn">Compare</button>
           </div>
 
           <div class = "social-links">
