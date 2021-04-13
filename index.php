f2498e3ac7c0dfcbeae61e1c73120c41b26ffa04
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-13 13:51:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -104,7 +104,7 @@
                         <h3>Company Name</h3>
                         <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
                     </div>
-                    <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                    <div class="col item social"><a href="#"><i class = "fab fa-whatsapp"></i></a><a href="#"><i class = "fab fa-whatsapp"></i></a><a href="#"><i class = "fab fa-whatsapp"></i></a><a href="#"><i class = "fab fa-whatsapp"></i></a></div>
                 </div>
                 <p class="copyright">Company Name © 2018</p>
             </div>
