42d7b261fcd9b92e66e3ed9f047329ea99b780a0
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-13 15:02:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -66,6 +66,7 @@
 						<p><?php echo $row['type']?></p>
 						<p><?php echo $row['price']?></p>
 					</a>
+					<h2 style="background-color: black; font-size: 23px; color: white; padding: 10px;" class="container">Best sales</h2>
 					<a href="single-product.php?id=<?php echo $row['productid']?>" class="product">
 						<div class="product-image"><img src="images/<?php echo $row['image']?>"></div>
 						<h2 class="product-title"><?php echo $row['name']?></h2>
