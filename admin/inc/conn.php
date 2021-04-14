ccd2ff03a44797854b4cdd2fc56b77e6dde4b053
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-04-14 16:06:05 +0700

    Update conn.php

--- admin/inc/conn.php
+++ admin/inc/conn.php
@@ -1,6 +1,4 @@
-<?php	 
-	$conn = pg_connect("host=ec2-52-21-252-142.compute-1.amazonaws.com dbname=dcle2fpc522se2 user=fmneqfpwrsmfic password=f2f4b403db219824f6af70a62a6cd17f982167cdc23ef004cf3ed53e403f98f4 port=5432");
-	if(!$conn){echo "Lost Connect";}
+<?php
 
 	$host = "host=ec2-52-21-252-142.compute-1.amazonaws.com";
 	$dbname = " dbname=dcle2fpc522se2";
@@ -8,5 +6,5 @@
 	$password = " password=f2f4b403db219824f6af70a62a6cd17f982167cdc23ef004cf3ed53e403f98f4 port=5432";
 
 	$conn = pg_connect($host.$dbname.$user.$password);
-	if(!$conn){echo "ERROR";}
+	if(!$conn){echo "LOST CONNECT";}
  ?>
